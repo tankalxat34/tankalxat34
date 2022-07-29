80930f647935f005b91f217de36ffc389a373544
Alexander Podstrechnyy <tankalxat34@gmail.com>
2022-07-29 10:11:59 +0300

    Delete main.py

--- someScripts/main.py
+++
@@ -1,7 +0,0 @@
-import os
-
-packageName = str(input("Package name: "))
-with open("badges.txt", "r", encoding="UTF-8") as file:
-    print(file.read().replace("PACKAGE_NAME", packageName))
-
-os.system("pause")
