0045d078bea4372066c052920cba2edd736e5045
Alexander Podstrechnyy <tankalxat34@gmail.com>
2023-01-25 15:25:42 +0300

    Add files via upload

---
+++ someScripts/createBages.py
@@ -0,0 +1,16 @@
+"""[![Downloads](https://pepy.tech/badge/uploadgrampyapi)](https://pepy.tech/project/uploadgrampyapi)
+[![Downloads](https://pepy.tech/badge/uploadgrampyapi/month)](https://pepy.tech/project/uploadgrampyapi)
+[![Downloads](https://pepy.tech/badge/uploadgrampyapi/week)](https://pepy.tech/project/uploadgrampyapi)
+[![Supported Versions](https://img.shields.io/pypi/pyversions/uploadgrampyapi.svg)](https://pypi.org/project/uploadgrampyapi)
+[![PyPI](https://img.shields.io/pypi/v/uploadgrampyapi.svg?color=success)](https://pypi.org/project/uploadgrampyapi/)
+[![PyPi](https://img.shields.io/pypi/format/uploadgrampyapi)](https://pypi.org/project/uploadgrampyapi/)
+![GitHub top language](https://img.shields.io/github/languages/top/tankalxat34/UploadgramPyAPI)
+[![GitHub last commit](https://img.shields.io/github/last-commit/tankalxat34/uploadgrampyapi)](https://github.com/tankalxat34/UploadgramPyAPI/commits/main)
+[![GitHub Release Date](https://img.shields.io/github/release-date/tankalxat34/UploadgramPyAPI)](https://github.com/tankalxat34/UploadgramPyAPI/releases)
+
+[![GitHub Repo stars](https://img.shields.io/github/stars/tankalxat34/UploadgramPyAPI?style=social)](https://github.com/tankalxat34/UploadgramPyAPI)
+"""
+
+import pyperclip
+
+pyperclip.copy(__doc__.lower().replace("uploadgrampyapi", "asjs"))
